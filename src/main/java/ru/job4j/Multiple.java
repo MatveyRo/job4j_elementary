a09c1b22b6d915311da61715d72ddcd486dc9a5a
MatveyRo <obzor.matvey@gmail.com>
2024-01-18 23:08:12 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+       // 1 * 2 = 2;
+       // 1 * 3 = 3;
+       // 1 * 4 = 4;
+       // 1 * 5 = 5;
+       // 1 * 6 = 6;
+       // 1 * 7 = 7;
+       // 1 * 8 = 8;
+        System.out.println("1 * 9 = 9");
+    }
+}
