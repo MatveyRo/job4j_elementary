2f191788001c4963bdecc9f1837f3fdfd517f21c
MatveyRo <obzor.matvey@gmail.com>
2024-01-27 16:18:28 +0300

    1.2. Входные данные в тесте [#297750 #476853]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -7,21 +7,56 @@ class X2Test {
 
     @Test
     void whenA10B0C0X2Then40() {
-        int a = 1, b = 1, c = 1, x = 1;
-        int a2 = 0, b2 = 1, c2 = 1, x2 = 1;
-        int a3 = 1, b3 = 1, c3 = 0, x3 = 1;
-        int a4 = 1, b4 = 1, c4 = 1, x4 = 0;
-        int expected = 3;
-        int expected2 = 2;
-        int expected3 = 2;
-        int expected4 = 1;
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
         int result = X2.calc(a, b, c, x);
-        int result2 = X2.calc(a2, b2, c2, x2);
-        int result3 = X2.calc(a3, b3, c3, x3);
-        int result4 = X2.calc(a4, b4, c4, x4);
         assertThat(result).isEqualTo(expected);
-        assertThat(result2).isEqualTo(expected2);
-        assertThat(result3).isEqualTo(expected3);
-        assertThat(result4).isEqualTo(expected4);
+    }
+
+    @Test
+    void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA1B1C1X0Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
     }
 }
