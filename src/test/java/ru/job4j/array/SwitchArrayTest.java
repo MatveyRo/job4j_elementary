89b9ba5037f858f64a0225b4c8fe6259105094a0
MatveyRo <obzor.matvey@gmail.com>
2024-02-05 19:59:22 +0300

    6.2.2. Переставить элементы массива [#226795]

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -6,34 +6,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SwitchArrayTest {
 
     @Test
-    void whenSwapBorderArrayLengthIs4() {
+    void whenSwap0to3() {
         int[] input = {1, 2, 3, 4};
-        int[] result = SwitchArray.swapBorder(input);
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
         int[] expected = {4, 2, 3, 1};
         assertThat(result).containsExactly(expected);
     }
 
     @Test
-    void whenSwapBorderArrayLengthIs6() {
-        int[] input = {1, 2, 3, 4, 5, 6};
-        int[] result = SwitchArray.swapBorder(input);
-        int[] expected = {6, 2, 3, 4, 5, 1};
+    void whenSwap1to2() {
+        int[] input = {1, 2, 4, 5};
+        int source = 1;
+        int destination = input.length - 2;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 4, 2, 5};
         assertThat(result).containsExactly(expected);
     }
 
     @Test
-    void whenSwapBorderArrayLengthIs3() {
-        int[] input = {1, 2, 3};
-        int[] result = SwitchArray.swapBorder(input);
-        int[] expected = {3, 2, 1};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    void whenSwapBorderArrayLengthIs1() {
-        int[] input = {1};
-        int[] result = SwitchArray.swapBorder(input);
-        int[] expected = {1};
+    void whenSwap2to3() {
+        int[] input = {1, 2, 4, 5, 6, 7};
+        int source = 2;
+        int destination = input.length - 3;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 2, 5, 4, 6, 7};
         assertThat(result).containsExactly(expected);
     }
 }
